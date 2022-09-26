ed5f836163221f099bbb252f7990f12322913fce
Doze42 <62532206+Doze42@users.noreply.github.com>
2022-09-26 13:31:24 -0600

    Fixed servers command

--- funcs/loadDefaults.js
+++ funcs/loadDefaults.js
@@ -6,7 +6,7 @@ const defaults = JSON.parse(fs.readFileSync("./assets/defaults.json")); //loads 
 async function addServer(id){
 	return new Promise(async(resolve, reject) => {
 		try{
-			await new sql.Request(global.pool).query("INSERT INTO SERVERS (SERVER_ID, EMBEDS, CONFIG) VALUES ('" + id + "', N'" + JSON.stringify(defaults.embeds) + "', N'" + JSON.stringify(defaults.config) + "')")
+			await new sql.Request(global.pool).query("INSERT INTO SERVERS (SERVER_ID, EMBEDS, CONFIG, SERVERS, LIVE) VALUES ('" + id + "', N'" + JSON.stringify(defaults.embeds) + "', N'" + JSON.stringify(defaults.config) + "', N'" + JSON.stringify(defaults.servers) + "', N'" + JSON.stringify(defaults.live) + "')")
 			resolve ('Server ' + id + ' added to database.')
 		}
 		catch(err){
