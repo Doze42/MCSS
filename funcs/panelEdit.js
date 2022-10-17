30852f6a8fba3e85ff49cf0f2d2ead4920d9777a
Doze42 <62532206+Doze42@users.noreply.github.com>
2022-10-16 23:34:14 -0600

    Promise based check function

--- funcs/panelEdit.js
+++ funcs/panelEdit.js
@@ -8,55 +8,58 @@ const sql = require('mssql')
 const compat = require ('../funcs/compat.js');
 
 async function check(element, stringJSON){
-try{
-	if (!global.statusCache.has(element.ip)){		
-		try {
-			var pingResults = await queryServer(element.ip)
-			var dbData = (await new sql.Request(global.pool).query('SELECT TOP 1 * from SERVERS WHERE SERVER_ID = ' + element.guildID)).recordset[0].COMPAT
-			if (await compat.check(pingResults, JSON.parse(dbData))){throw stringJSON.status.compatOffline;};
-			global.statusCache.set(element.ip, {online: true, data: pingResults})}
-		catch(err){
-		global.statusCache.set(element.ip, {online: false, data: err})}
-	}
+return new Promise(async(resolve, reject) => {
+	try{
+		if (!global.statusCache.has(element.ip)){		
+			try {
+				var pingResults = await queryServer(element.ip)
+				var dbData = (await new sql.Request(global.pool).query('SELECT TOP 1 * from SERVERS WHERE SERVER_ID = ' + element.guildID)).recordset[0].COMPAT
+				if (await compat.check(pingResults, JSON.parse(dbData))){throw stringJSON.status.compatOffline;};
+				global.statusCache.set(element.ip, {online: true, data: pingResults})}
+			catch(err){
+			global.statusCache.set(element.ip, {online: false, data: err})}
+		}
 
-	if ((new Date().getTime() - element.lastPing) >= 1209600000){
-			//todo: make the disable time configurable
-			
-		var disable = true;
-		var statEmbed = new Discord.MessageEmbed() 
-			.setTitle(stringJSON.automsg.disabled.header)
-			.setColor('E74C3C')
-			.setDescription(stringJSON.automsg.disabled.body)
-			.setFooter({text: stringJSON.embeds.footerText})
-			.setTimestamp()
-		if(element.embedTemplate.thumbnailEnable){statEmbed.setThumbnail('attachment://favicon.png')}
-	}
-	else {
-		if (global.statusCache.get(element.ip).online){
-			var statEmbed = richEmbeds.statusEmbed({
-			online: true,
-			favicon: 'favicon.png',
-			data: global.statusCache.get(element.ip).data,
-			format: element.embedTemplate
-			}, stringJSON);
+		if ((new Date().getTime() - element.lastPing) >= 1209600000){
+				//todo: make the disable time configurable
+				
+			var disable = true;
+			var statEmbed = new Discord.MessageEmbed() 
+				.setTitle(stringJSON.automsg.disabled.header)
+				.setColor('E74C3C')
+				.setDescription(stringJSON.automsg.disabled.body)
+				.setFooter({text: stringJSON.embeds.footerText})
+				.setTimestamp()
+			if(element.embedTemplate.thumbnailEnable){statEmbed.setThumbnail('attachment://favicon.png')}
 		}
 		else {
-			var statEmbed = richEmbeds.statusEmbed({
-			online: false,
-			favicon: 'favicon.png',
-			data: {error: global.statusCache.get(element.ip).data, hostname: element.ip},
-			format: element.embedTemplate
-			}, stringJSON);
+			if (global.statusCache.get(element.ip).online){
+				var statEmbed = richEmbeds.statusEmbed({
+				online: true,
+				favicon: 'favicon.png',
+				data: global.statusCache.get(element.ip).data,
+				format: element.embedTemplate
+				}, stringJSON);
+			}
+			else {
+				var statEmbed = richEmbeds.statusEmbed({
+				online: false,
+				favicon: 'favicon.png',
+				data: {error: global.statusCache.get(element.ip).data, hostname: element.ip},
+				format: element.embedTemplate
+				}, stringJSON);
+			}
 		}
+			if(!isEqual({title: statEmbed.title.toString(), description: statEmbed.description.toString(), fields: statEmbed.fields, footer: statEmbed.footer.text.toString()},
+			{title: element.lastState.title.toString(), description: element.lastState.description.toString(), fields: element.lastState.fields, footer: element.lastState.footer.text.toString()}))
+			{resolve ({update: true, data: statEmbed, disable: disable})}
+			else {resolve ({update: false, disable: disable});}
 	}
-		if(!isEqual({title: statEmbed.title.toString(), description: statEmbed.description.toString(), fields: statEmbed.fields, footer: statEmbed.footer.text.toString()},
-		{title: element.lastState.title.toString(), description: element.lastState.description.toString(), fields: element.lastState.fields, footer: element.lastState.footer.text.toString()}))
-		{return {update: true, data: statEmbed, disable: disable}}
-		else {return {update: false, disable: disable};}
-}
-catch(err){
-	global.toConsole.error('Failed at panelEdit.check with panel ID ' + element.messageID + ' : ' + err);
-}
+	catch(err){
+		global.toConsole.error('Failed at panelEdit.check with panel ID ' + element.messageID + ' : ' + err);
+		reject(err)
+	}
+})
 }
 
 function update(data, client, stringJSON){
